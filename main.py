27e2346a9c60ca27ff0c0f5122d29b4177e6c250
poberman11 <133469122+poberman11@users.noreply.github.com>
2025-06-13 21:23:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+# main.py
+import sys
+from PySide6.QtWidgets import QApplication
+from ui.login import LoginWindow
 
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    # Crear e iniciar la ventana de login
+    window = LoginWindow()
+    window.showFullScreen()
+
+    sys.exit(app.exec())
