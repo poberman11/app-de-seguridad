984c99f8cce8b9290b00a8db789adc779fcc79f3
poberman11 <133469122+poberman11@users.noreply.github.com>
2025-06-13 21:25:55 -0400

    Update security.py

--- core/security.py
+++ core/security.py
@@ -1,1 +1,8 @@
+# core/security.py
+import bcrypt
 
+def hash_password(password: str) -> bytes:
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+def check_password(password: str, hashed: bytes) -> bool:
+    return bcrypt.checkpw(password.encode(), hashed)
